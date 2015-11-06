d9aed3cbc3741dd38b48d1ad3d6c1c7f23f710e3
Alec Larsen <alec.larsen@agapered.com>
2015-11-06 14:59:01 -0600

    Hopefully makes travis work now

--- spec/hipaapotamus_spec.rb
+++ spec/hipaapotamus_spec.rb
@@ -4,8 +4,4 @@ describe Hipaapotamus do
   it 'has a version number' do
     expect(Hipaapotamus::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
