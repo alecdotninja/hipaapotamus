5b70407e70ad62e502e96965a2955d64b468e42a
Alec Larsen <alec.larsen@agapered.com>
2015-11-12 00:51:18 -0600

    Adds #with_accountability to agent

--- lib/hipaapotamus/agent.rb
+++ lib/hipaapotamus/agent.rb
@@ -1,7 +1,12 @@
 require 'active_support/concern'
+require 'hipaapotamus/accountability_context'
 
 module Hipaapotamus
   module Agent
     extend ActiveSupport::Concern
+
+    def with_accountability(&block)
+      AccountabilityContext.new(self, &block)
+    end
   end
 end
---
+++ spec/hipaapotamus/agent_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Hipaapotamus::Agent do
+  let(:agent) { User.create! }
+
+  context 'instance methods' do
+    describe '#with_accountability' do
+      it 'evaluates a block in an AccountabilityContext with the agent' do
+        agent.with_accountability do
+          expect(Hipaapotamus::AccountabilityContext.current.agent).to eq agent
+        end
+      end
+    end
+  end
+end
